0cf25ad8ddf7fee9a1c7674189006847c646e66d
Tingxuan Gu <gutingxuan@outlook.com>
2021-04-09 00:00:00 -0400

    message

--- commit.sh
+++ commit.sh
@@ -1,7 +1,6 @@
 echo $1 $2 >> commit_history
 month=$1
 day=$2
-echo $month >> commit_history
 git add .
-git commit --date="2021-'$1'-'$2' 00:00:00" -m "message"
+git commit --date="2021-'$month'-'$day' 00:00:00" -m "message"
 
